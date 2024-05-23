22f62f9969f9c061d80aeb863a15b20f7ed24a50
hqanhh <huynhquynhanh2003@gmail.com>
2024-05-23 15:22:49 +0800

    fix bug

--- core/train_t5.py
+++ core/train_t5.py
@@ -7,6 +7,7 @@ from utils.utils import *
 from core.worker import train
 from core.network import Network
 from datasets.preprocessing_t5 import SrcLang, TgtLang, get_raw_pairs, Collater
+from datasets import get_dataloader
 
 # Utility functions
 def set_seed(args):
@@ -21,38 +22,11 @@ def get_language(args):
     tgt_lang = TgtLang(args.vocab_tgt_path)
     return src_lang, tgt_lang
 
-class CustomDataset(Dataset):
-    def __init__(self, raw_pairs, src_lang, tgt_lang):
-        self.raw_pairs = raw_pairs
-        self.src_lang = src_lang
-        self.tgt_lang = tgt_lang
-
-    def __len__(self):
-        return len(self.raw_pairs)
-
-    def __getitem__(self, idx):
-        sample = self.raw_pairs[idx]
-        src_input = self.src_lang.indexes_from_sentence(sample['text'], id_type='text')
-        tgt_input = self.tgt_lang.indexes_from_sentence(sample['expression'])
-        return src_input, tgt_input
-
-def get_dataloader(args):
-    raw_pairs = get_raw_pairs(args.dataset_dir)
-    train_size = int(0.8 * len(raw_pairs))
-    val_size = len(raw_pairs) - train_size
-    train_dataset = CustomDataset(raw_pairs[:train_size], args.src_lang, args.tgt_lang)
-    val_dataset = CustomDataset(raw_pairs[train_size:], args.src_lang, args.tgt_lang)
-
-    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, collate_fn=Collater(args, args.src_lang))
-    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, collate_fn=Collater(args, args.src_lang))
-
-    return train_loader, None, val_loader
-
 # Training script
 def main_worker(args):
     set_seed(args)
     src_lang, tgt_lang = get_language(args)
-    train_loader, train_sampler, val_loader = get_dataloader(args)
+    train_loader, train_sampler, val_loader, src_lang, tgt_lang = get_dataloader(args)
     model = Network(args, src_lang, tgt_lang).cuda()
     optimizer = AdamW(model.parameters(), lr=args.learning_rate)
     total_steps = len(train_loader) * args.num_train_epochs
