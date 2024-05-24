d53758dc7cf8d5f379db7e9e12f803f53800a280
hqanhh <huynhquynhanh2003@gmail.com>
2024-05-24 12:56:00 +0800

    fix bug

--- core/train_t5.py
+++ core/train_t5.py
@@ -4,7 +4,7 @@ import torch
 from torch.utils.data import DataLoader, Dataset
 from transformers import T5ForConditionalGeneration, AdamW, get_linear_schedule_with_warmup
 from utils.utils import *
-from core.worker import train
+from core.worker_t5 import train
 # from core.network_t5 import Network
 from datasets.preprocessing_t5 import SrcLang, TgtLang, get_raw_pairs, Collater
 from datasets import get_dataloader
--- datasets/__init__.py
+++ datasets/__init__.py
@@ -30,6 +30,41 @@ import os
 
 from transformers import T5Tokenizer
 
+# def get_dataloader(args):
+#     tokenizer = T5Tokenizer.from_pretrained('google/flan-t5-base')
+#     src_lang = SrcLang(args.vocab_src_path)
+#     tgt_lang = TgtLang(args.vocab_tgt_path)
+
+#     train_data_path = os.path.join(args.dataset_dir, args.dataset, 'train.json')
+#     train_pairs = get_raw_pairs(train_data_path)
+#     test_data_path = os.path.join(args.dataset_dir, args.dataset, 'test.json')
+#     test_pairs = get_raw_pairs(test_data_path)
+
+#     train_pairs += test_pairs
+
+#     def tokenize_function(examples):
+#         model_inputs = tokenizer(examples['src'], max_length=args.max_seq_length, truncation=True)
+#         labels = tokenizer(examples['tgt'], max_length=args.max_seq_length, truncation=True)
+#         model_inputs["labels"] = labels["input_ids"]
+#         return model_inputs
+
+#     train_data = MyDataset(args, train_pairs, src_lang, tgt_lang, is_train=True)
+#     train_data = train_data.map(tokenize_function, batched=True)
+
+#     train_sampler = DistributedSampler(train_data, shuffle=True)
+#     train_loader = DataLoader(dataset=train_data, 
+#                               batch_size=int(args.batch_size / args.nprocs), 
+#                               pin_memory=True, 
+#                               collate_fn=collater(args, src_lang), 
+#                               num_workers=args.workers, 
+#                               sampler=train_sampler
+#                              )
+                            
+#     return train_loader, train_sampler, None, src_lang, tgt_lang
+
+from torch.utils.data import DataLoader, DistributedSampler
+from transformers import T5Tokenizer
+
 def get_dataloader(args):
     tokenizer = T5Tokenizer.from_pretrained('google/flan-t5-base')
     src_lang = SrcLang(args.vocab_src_path)
@@ -42,20 +77,21 @@ def get_dataloader(args):
 
     train_pairs += test_pairs
 
-    def tokenize_function(examples):
-        model_inputs = tokenizer(examples['src'], max_length=args.max_seq_length, truncation=True)
-        labels = tokenizer(examples['tgt'], max_length=args.max_seq_length, truncation=True)
-        model_inputs["labels"] = labels["input_ids"]
-        return model_inputs
-
     train_data = MyDataset(args, train_pairs, src_lang, tgt_lang, is_train=True)
-    train_data = train_data.map(tokenize_function, batched=True)
+
+    def collate_fn(batch):
+        input_texts = [item['text'] for item in batch]
+        target_texts = [item['target'] for item in batch]
+        model_inputs = tokenizer(input_texts, max_length=args.max_seq_length, truncation=True, padding="max_length", return_tensors="pt")
+        labels = tokenizer(target_texts, max_length=args.max_seq_length, truncation=True, padding="max_length", return_tensors="pt").input_ids
+        model_inputs["labels"] = labels
+        return model_inputs
 
     train_sampler = DistributedSampler(train_data, shuffle=True)
     train_loader = DataLoader(dataset=train_data, 
                               batch_size=int(args.batch_size / args.nprocs), 
                               pin_memory=True, 
-                              collate_fn=collater(args, src_lang), 
+                              collate_fn=collate_fn, 
                               num_workers=args.workers, 
                               sampler=train_sampler
                              )
